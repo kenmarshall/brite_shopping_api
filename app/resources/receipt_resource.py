c2196f0f2053ea8ebc698fc60c992fd0f361b21b
Kenneth Marshall <kdmarshall89@gmail.com>
2024-11-20 08:43:00 -0500

    add extract_price_info_from_receipt_image

--- app/resources/receipt_resource.py
+++ app/resources/receipt_resource.py
@@ -1,9 +1,15 @@
+import io
+import logging
 from flask_restful import Resource
-from flask import request
+from flask import request, jsonify
 from app.tasks import receipt_task
+from app.services import extract_price_info_from_receipt_image
+from PIL import Image
 
 
 ACCEPTED_IMAGE_FILE_EXTENSIONS = {"jpg", "jpeg", "png", "heic", "heif", "webp"}
+
+logger = logging.getLogger(__name__)
 
 
 def image_file_extension_is_allowed(filename):
@@ -15,17 +21,24 @@ def image_file_extension_is_allowed(filename):
 
 class ReceiptResource(Resource):
     def post(self):
-      try:
-        # file = request.files.get("file")
 
-        # if not file or file.filename == "":
-        #     return {"message": "image file not found"}, 400
+        try:
+            file = request.files.get("file")
 
-        # if not image_file_extension_is_allowed(file.filename):
-        #     return {"message": "file extension is not allowed"}, 400
-        receipt_task.process.delay("Processed in the background")
-        # Store file in temporary queue for processing
-      except Exception as e:
+            if not file or file.filename == "":
+                logger.info("image file not found")
+                return {"message": "image file not found"}, 400
+
+            if not image_file_extension_is_allowed(file.filename):
+                logger.info(f"file extension is not allowed {file.filename}")
+                return {"message": "file extension is not allowed"}, 400
+
+            image = Image.open(io.BytesIO(file.read()))
+
+            extracted_text = extract_price_info_from_receipt_image(image)
         
-        return {"message": f"An error occurred {e}"}, 500
-      
+            return jsonify({"extracted_text": extracted_text})
+            # receipt_task.process.delay("Processed in the background")
+        except Exception as e:
+
+            return {"message": f"An error occurred {e}"}, 500
